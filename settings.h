79cd5b268ecbcaecfe35d8a18876b128d2977844
roccotsi <37236549+roccotsi@users.noreply.github.com>
2018-06-24 10:17:11 +0200

    Updated

--- settings.h
+++ settings.h
@@ -5,11 +5,11 @@
 #define Settings_h
 
 // WLAN settings
-#define WLAN_SSID                      "2006"
-#define WLAN_PASSWORD                  "H644G845P1181"
+#define WLAN_SSID                      "???"
+#define WLAN_PASSWORD                  "???"
 
 // weather settings (for OPEN_WEATHER_MAP_APP_ID you need to subcribe to openweathermap.org)
-#define OPEN_WEATHER_MAP_APP_ID        "5c08c31950b12a7114fd3e6913903602"
+#define OPEN_WEATHER_MAP_APP_ID        "???"
 #define WEATHER_CITY                   "Herten"
 #define WEATHER_UPDATE_INTERVAL_MILLIS 600000; // aprox. every 10 minutes
 
